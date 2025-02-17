9491771cfe0ddc542fa83e123ff7eff27e2fbb79
BugHunter-X <askzenai@gmail.com>
2025-02-17 14:14:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// SomeComponent.js
+<div className="flex items-center">
+  <div className="bg-gray-200 rounded-full p-2 mr-2">
+    {/* ... some content ... */}
+  </div>
+  <p>Tailwind doesn't apply styles to this p tag.</p>
+</div>
+```
